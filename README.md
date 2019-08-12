27dd3727cbfc51cd6d80f66c3fb1eef61e86e2dd
Anton Abilov <anton_abilov@hotmail.com>
2019-08-12 06:30:38 -0700

    Updated

--- README.md
+++ README.md
@@ -6,12 +6,15 @@ The ideas were presented at JSConf US so it's the best to explore the code in th
 This is mainly meant to supplement the talk. We're currently in the process of packaging some of these methods and patterns as a library with proper tests and documentation.
 
 ## Code snippets
-- [action$](https://github.com/anton164/everything-as-a-stream/blob/master/streams/action%24.ts)
-- [Routines]()
-- [state$]()
-- [connect]() _Connect a React view to a stream_
-- [createBackboneModelStream] _Create a proxy state observable from a Backbone model_
+- [action$](https://github.com/anton164/everything-as-a-stream/blob/master/streams/action%24.ts) - _the action stream_
+- [Routines]() - _Routines example_
+- [state$]() - _state$ example_
+
+## Utility methods
+- [createStateStream]() - _API for centralizing state stream creation_
+- [connect]() - _connect a React view to a stream_
+- [createBackboneModelStream]() - _create a proxy state observable from a Backbone model_
 
 ## Custom operators
-- [ofType]() Filter actions emitted from the action$ by a certain type
-- [catchErrorLogAndContinue]() Catch an error, log it then keep the stream alive. Useful to make your state streams and routines more robust.
+- [ofType]() - _filter actions emitted from the action$ by a certain type_
+- [catchErrorLogAndContinue]() - _keeps your stream alive on error, useful to make your streams more robust_
--- snippets/stateStreamUtils.ts
+++ snippets/stateStreamUtils.ts
@@ -1,5 +1,7 @@
 import { Observable, Subscriber } from 'rxjs';
 import { shareReplay } from 'rxjs/operators';
+import { catchErrorLogAndContinue } from './operators';
+import { tag } from 'rxjs-spy/operators';
 
 /**
  * 
@@ -9,7 +11,7 @@ import { shareReplay } from 'rxjs/operators';
  * The stream will emit the model attributes
  * when the backbone model is synced with the server.
  * 
- */ 
+ */
 export const createBackboneModelStream = <T>(
   model: Backbone.Model
 ): Observable<T> =>
@@ -18,3 +20,36 @@ export const createBackboneModelStream = <T>(
     model.on('sync', () => observer.next(model.toJSON()), observer);
     return () => model.off(undefined, undefined, observer);
   }).pipe(shareReplay({ bufferSize: 1, refCount: true }));
+
+type StateStreamOptions = {
+  debugName: string;
+};
+
+type CreateStateStream<StateShape> = StateStreamOptions & {
+  createStream(...streams: Observable<any>[]): Observable<StateShape>;
+};
+
+/**
+ *
+ * API for creating a state stream
+ *
+ * This allows you to provide a createStream method
+ * that takes an arbitrary number of streams and returns a stream creator
+ *
+ * The API ensures that the state stream is hot, tagged with RxJS spy
+ * and adds "catchErrorLogAndContinue" to ensure that
+ * the stream lives on when it throws an error.
+ *
+ */
+export function createStateStream<StateShape>({
+  debugName,
+  createStream,
+}: CreateStateStream<StateShape>) {
+  return (...streams: Observable<any>[]) => {
+    return createStream(...streams).pipe(
+      catchErrorLogAndContinue(),
+      shareReplay({ bufferSize: 1, refCount: true }),
+      tag(`state$-${debugName}`)
+    );
+  };
+}
